2dcb7b9f56c64e2b5dfbcd144762b34a45ebb996
Daniel Jonas <02danieljonas@gmail.com>
2022-06-08 19:36:03 -0400

    finished add more iamges

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const API_KEY = "";
 
 let q = [];
 
@@ -5,7 +6,7 @@ let limit = 11;
 
 let offset = 0;
 
-let rating = "g";
+let rating = "PG";
 
 let lang = "en";
 
@@ -13,27 +14,45 @@ let searchBar = document.querySelector("#giphy-search");
 
 let form = document.querySelector("form");
 
+let loadButton = document.querySelector("#loadButton");
+
 let imgContainer = document.querySelector("#image-container");
 
 let img;
 let image;
 let offsetList = [];
 let random;
+let searchTerms;
 
 function parseSearch(e) {
-    imgContainer.style.backgroundColor="bisque"
-    imgContainer.childNodes.forEach((e) => {
-        e.remove();
-    });
     e.preventDefault();
-
     if (searchBar.value == "") {
-        searchBar.value = "pizza, chicken";
+        return
     }
-    let searchTerms = searchBar.value.split(", ");
+    imgContainer.style.backgroundColor="bisque"
+    loadButton.style.display="revert"
+    imgContainer.childNodes.forEach((element) => {
+        element.childNodes[0].remove()
+        element.remove();
+    });
+
+    searchTerms = searchBar.value.split(", ");
     for (let i = 0; i <= limit-1; i++) {
+        let j = Math.floor(Math.random() * searchTerms.length)
+        console.log(searchTerms[j])
         getGiphyData(
-            searchTerms[Math.floor(Math.random() * searchTerms.length)]
+            searchTerms[j]
+        );
+    }
+}
+
+function loadMore(){
+    limit+=11
+    for (let i = 0; i <= limit-1; i++) {
+        let j = Math.floor(Math.random() * searchTerms.length)
+        console.log(searchTerms[j])
+        getGiphyData(
+            searchTerms[j]
         );
     }
 }
@@ -45,23 +64,17 @@ async function getGiphyData(whatToSearch) {
     offsetList.push(random);
     let link = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${whatToSearch}&limit=1&offset=${
         offsetList[offsetList.length - 1]
-    }&rating=${rating}&lang=${lang}`;
+    }`;
     let response = await fetch(link);
     let responseData = await response.json();
     responseData.data.forEach((e) => {
         img = document.createElement("img");
         image = document.createElement("div");
-        console.log(image)
         image.appendChild(img);
         imgContainer.appendChild(image);
         img.src = e.images.original.url;
     });
 }
 
-window.onscroll = (e)=>{
-    console.log(window.scrollY)
-    console.log(document.body.scrollHeight)
-}
-
-
 form.addEventListener("submit", parseSearch);
+loadButton.addEventListener("click", loadMore);
